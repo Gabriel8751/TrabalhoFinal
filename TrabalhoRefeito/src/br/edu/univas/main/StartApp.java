4e506edd9058908b7bec8d81f6d46eb4b61c158c
Gabriel <97056190+Gabriel8751@users.noreply.github.com>
2022-04-27 13:01:43 -0300

    Update StartApp.java

--- TrabalhoRefeito/src/br/edu/univas/main/StartApp.java
+++ TrabalhoRefeito/src/br/edu/univas/main/StartApp.java
@@ -10,25 +10,25 @@ public class StartApp {
 	public static void main(String[] args) {
 		Scanner leitura = new Scanner(System.in);
 		ListaDupla lista = new ListaDupla();
-		int começar = 0;
+		int comeÃ§ar = 0;
 		lista.todasPecas();
 		lista.PecasPlayer();
 		lista.PecasComputador();
 		do {
-			if(lista.whoStart() == 0 && começar == 0) {
-				System.out.println("Escolha uma peça para retirar");
+			if(lista.whoStart() == 0 && comeÃ§ar == 0) {
+				System.out.println("Escolha uma peÃ§a para retirar");
 				System.out.println(lista.ListaPecasPlayer());
 				int select = leitura.nextInt();
 				leitura.nextLine();
 				lista.sendPiecePlayer(select);
-				começar++;
-			}else if(lista.whoStart() == 1 && começar == 0) {
+				comeÃ§ar++;
+			}else if(lista.whoStart() == 1 && comeÃ§ar == 0) {
 				lista.firstPlay();
 				System.out.println(lista.listPiece()+"\n");
-				começar++;
+				comeÃ§ar++;
 			}
 			else {
-				System.out.println("Escolha uma peça para retirar");
+				System.out.println("Escolha uma peÃ§a para retirar");
 				System.out.println(lista.ListaPecasPlayer());
 				int select = leitura.nextInt();
 				leitura.nextLine();
@@ -36,15 +36,15 @@ public class StartApp {
 				System.out.println("\n"+lista.listPiece()+"\n");
 				lista.sendPieceComputer();
 				System.out.println("\n"+lista.listPiece()+"\n");
-				if(lista.verifyPlayer() == 9 || lista.verifyComputer() == 9) {
+				if(lista.verifyPlayer() == 0 || lista.verifyComputer() == 0) {
 					break;
 				}
 			}
 		}while(true);
 		if(lista.verifyPlayer() == 0) {
-			System.out.println("Parabens você ganhou!");
-		}else if(lista.verifyComputer() == 0) {
-			System.out.println("Não foi dessa vez!");
+			System.out.println("Parabens vocÃª ganhou!");
+		}else {
+			System.out.println("NÃ£o foi dessa vez!");
 		}
 	}
 }
